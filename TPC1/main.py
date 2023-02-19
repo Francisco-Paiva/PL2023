5ad377a32f6a10c2f37c57d1b041942cde18c75f
Francisco Paiva <61988754+Francisco-Paiva@users.noreply.github.com>
2023-02-19 20:24:42 +0000

    TPC1 Versão Final

--- TPC1/main.py
+++ TPC1/main.py
@@ -1,186 +1,103 @@
-import io
-import re
-from collections import OrderedDict
-#import matplotlib
+import matplotlib.pyplot as plt
 
-data = OrderedDict()
 data1 = []
+keys = []
 
 def leitura():
     f = open("TPC1\\myheart.csv","r",encoding = 'utf-8')
-    #file = io.open("TPC1\\myheart.csv","r")
     line0 = f.readline();
-    #print(line0)
-    keys = re.split(r',',line0)
-    #print(keys)
-    num_attr = len(keys)
-    #print(num_attr)
-    pattern = re.compile(r'^[1-9][0-9]*\,(M|F)\,[0-9]+\,[0-9]+\,[0-9]+\,(0|1)$',re.UNICODE)
-    #i=0
+    keys = line0.split("\n")[0].split(",")
     for line in f:
-        r = re.search(pattern,line)
-        sep = re.split(r',',r.group(0))
+        r = line.split("\n")[0].split(",")
         data_tmp = []
-        data_tmp.append(int(sep[0]))
-        data_tmp.append(sep[1])
-        data_tmp.append(int(sep[2]))
-        data_tmp.append(int(sep[3]))
-        data_tmp.append(int(sep[4]))
-        data_tmp.append(int(sep[5]))
-        #data_tmp[0] = sep[0]
-        #data_tmp[1] = sep[1]
+        data_tmp.append(int(r[0]))
+        data_tmp.append(r[1])
+        data_tmp.append(int(r[2]))
+        data_tmp.append(int(r[3]))
+        data_tmp.append(int(r[4]))
+        data_tmp.append(int(r[5]))
         data1.append(data_tmp)
-        #i+=1
-    #print(data1)
-    '''for line in f:
-        print(line)'''
-    '''for linha in f:
-        print(linha)'''
-
-leitura()
-
-num_masculino_total = 0
-num_feminino_total = 0
-num_masculino_positivo = 0
-num_feminino_positivo = 0
-num_masculino_negativo = 0
-num_feminino_negativo = 0
-
-def distribuicao_sexo():
-    for case in data1:
-        '''if case[1] == 'M':
-            num_masculino_total+=1
-            if case[5] == 1:
-                num_masculino_positivo+=1'''
-        if case[1] == 'M' and case[5] == 1:
-            num_masculino_positivo+=1
-        elif case[1] == 'F' and case[5] == 1:
-            num_feminino_positivo+=1
-    print("Número de Casos Masculinos Positivos é: " + str(num_masculino_positivo))
-    print("Número de Casos Femininos Positivos é: " + str(num_feminino_positivo))
     return;
 
-#distribuicao_sexo()
-
-''' Função auxiliar '''
-def maior():
-    maior=0
+def distribuicao_sexo():
+    s = [[0,0],[0,0]]
     for case in data1:
-        if maior < case[0]:
-            maior = case[0]
-    print(maior)
-
-#maior()
-
-''' Função auxiliar '''
-def menor():
-    menor=data1[0][0]
-    for case in data1:
-        if menor > case[0]:
-            menor = case[0]
-    print(menor)
-
-#menor()
-
-'''Função auxiliar'''
-def aux():
-    lista = []
-    lista.append(1)
-    lista[0] += 4
-    print(lista)
-
-#aux()
+        if case[1] == 'M':
+            if case[5] == 1:
+                s[0][0]+=1
+            else: s[0][1]+=1
+        else:
+            if case[5] == 1:
+                s[1][0]+=1
+            else: s[1][1]+=1
+    return ["\tSexo\t", "Casos Positivos", "Casos Negativos"], ["M", "F"], s;
 
 def distribuicao_etarios():
     faixas = []
-    i = 0
+    intervalos = []
+    i = 0#30
     while i < 10:
         faixas.append([0,0])
-        i+=1
+        intervalos.append( ""+ str((i*5)+30) + "-" + str((i*5)+34) )
+        i+=1#i+=10
     for case in data1:
         ind =int((case[0]-30)/5)
         if case[5] == 1:
-            faixas[ind][1]+=1
-        else:
             faixas[ind][0]+=1
-    #print(faixas)
-
-    '''for case in data1:
-        if case[0] >= 30 and case[0] <= 34:
-            if case[5] == 1:
-                faixa1_positivos+=1
-            else: faixa1_negativos+=1
-        if case[0] >= 35 and case[0] <= 39:
-            if case[5] == 1:
-                faixa2_positivos+=1
-            else: faixa2_negativos+=1
-        if case[0] >= 40 and case[0] <= 44:
-            if case[5] == 1:
-                faixa3_positivos+=1
-            else: faixa3_negativos+=1
-        if case[0] >= 45 and case[0] <= 49:
-            if case[5] == 1:
-                faixa4_positivos+=1
-            else: faixa4_negativos+=1
-        if case[0] >= 50 and case[0] <= 54:
-            if case[5] == 1:
-                faixa5_positivos+=1
-            else: faixa5_negativos+=1
-        if case[0] >= 55 and case[0] <= 59:
-            if case[5] == 1:
-                faixa3_positivos+=1
-            else: faixa3_negativos+=1
-        if case[0] >= 60 and case[0] <= 64:
-            if case[5] == 1:
-                faixa3_positivos+=1
-            else: faixa3_negativos+=1
-        if case[0] >= 65 and case[0] <= 69:
-            if case[5] == 1:
-                faixa3_positivos+=1
-            else: faixa3_negativos+=1
-        if case[0] >= 70 and case[0] <= 74:
-            if case[5] == 1:
-                faixa3_positivos+=1
-            else: faixa3_negativos+=1
-        if case[0] >= 75 and case[0] <= 79:
-            if case[5] == 1:
-                faixa3_positivos+=1
-            else: faixa3_negativos+=1'''
-    #faixas.append()
-    #print("Entre 30 e 34 anos temos: " + str(faixa1_positivos) + " casos positivos e " + str(faixa1_negativos) + " casos negativos.")
-    return "", faixas;
-
-#distribuicao_etarios()
+        else:
+            faixas[ind][1]+=1
+    return ["Intervalo de Idade","Casos Positivos", "Casos Negativos"], intervalos, faixas;
 
 def distribuicao_colestrol():
+    intervalo = []
+    casos = []
     maior = data1[0][3]
     menor = data1[0][3]
     for case in data1:
-        if maior < case[3]:
+        if maior < case[3] and case[3]!=0:
             maior = case[3]
-        if menor > case[3]:
+        if menor > case[3] and case[3]!=0:
             menor = case[3]
-    #print("Maior: " + str(maior) + " Menor: " + str(menor))
-    faixas = []
-    #while 
-    #faixas.append((0,0))
-    print(faixas)
+    i=0
+    num = (maior-menor)/10
+    while i < num+1:
+        intervalo.append( ""+ str((i*10)+menor) + "-" + str((i*10)+menor+9) )
+        casos.append([0,0])
+        i+=1
+    for c in data1:
+        ind =int((c[3]-menor)/10)
+        if c[5] == 1:
+            casos[ind][0]+=1
+        else: casos[ind][1]+=1
+    return ["Nivel de Colestrol","Casos Positivos", "Casos Negativos"], intervalo, casos;
+
+def distribuicao_printTabela(keys):
+    first_line, lista_esquerda, lista_dados = keys
+    str1 = "|"
+    for x in first_line:
+        str1 = str1 + "  " + x + "  |"
+    print(str1)
+    i = 0
+    for case in lista_dados:
+        str2 = "|\t" + lista_esquerda[i] + "\t\t|" + "  \t" + str(case[0]) + "  \t|\t" + str(case[1]) + "  \t|"
+        print(str2)
+        i+=1
     return;
 
-#distribuicao_colestrol()
-
-def distribuicao_printTabela(keys,lis):
-    '''string = ""
-    for case in keys:
-        string.join(case + "\t")'''
-    print(keys)#print(string)
-    '''for case in lis:
-        print(case)'''
-    return;
-
+'''Programa'''
+leitura()
+distribuicao_printTabela(distribuicao_sexo())
 distribuicao_printTabela(distribuicao_etarios())
+distribuicao_printTabela(distribuicao_colestrol())
 
-#programa
+'''extra'''
+'''def graph_(l0,l1,l2,l3):
+    fig, ax = plt.subplots()
+    ax.bar(l2, l3)
+    ax.set_xlabel(l0[0])
+    ax.set_title(l0[1])
+    plt.show()'''
 
-
-#extra
+#graph_(["Sexo","Distribuição da Doença por Sexo"],distribuicao_sexo())
+#graph_(["Intervalo de Idades","Distribuição da Doença por Escalões Etários"],distribuicao_etarios())
+#graph_(["Intervalo de valores do colesterol","Distribuição da Doença por Níveis de Colesterol"],distribuicao_colestrol())
