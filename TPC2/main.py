26f4333d28566d3a3e08feab486cb390ec7638c7
Francisco Paiva <61988754+Francisco-Paiva@users.noreply.github.com>
2023-02-22 13:56:11 +0000

    TPC2 final(acho)

---
+++ TPC2/main.py
@@ -0,0 +1,28 @@
+import sys
+
+soma = 0
+boo = True
+separadores = ['_', ' ', '-', ';', ',']
+separador = ""
+
+for line in sys.stdin:
+    elements = []
+    if separador == "" or separador in separadores:
+        for x in separadores:
+            if x in line:
+                separador = x
+        elements = line.split(separador)
+    else :
+        elements = [line]
+    elements[len(elements)-1] = elements[len(elements)-1].split("\n")[0]
+    for elem in elements:
+        if   "off" == elem.lower() and boo:
+            boo = False
+        elif "on" == elem.lower() and (not boo):
+            boo = True
+            continue
+        elif "=" == elem:
+            print(soma)
+            sys.exit()
+        if boo:
+            soma += float(elem)
